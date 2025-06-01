28430c02281fddd491d49f9183e3b53804ed5cf1
jmagwili <joshuamagwili@gmail.com>
2025-06-01 12:09:09 +0800

    code clean-up and added sensor data to post request

--- IoTThesis.ino
+++ IoTThesis.ino
@@ -7,6 +7,9 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+const String API = "http://192.168.0.104:5114";
+const int deviceID = 1;
+
 void setup() {
   // Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
@@ -25,69 +28,43 @@ void setup() {
 }
 
 void loop() {
-
-   // Wait a few seconds between measurements.
   delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(t)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(API + "/api/iot"); // Replace with your server URL
+    http.addHeader("Content-Type", "application/json");
 
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
-  // if (WiFi.status() == WL_CONNECTED) {
-  //   HTTPClient http;
-  //   http.begin("http://192.168.0.100:5114/api/iot"); // Replace with your server URL
-  //   http.addHeader("Content-Type", "application/json");
+    int deviceID = 2;
 
-  //   int deviceID = 2;
-  //   int humidity = 50;
-  //   int temp = 22;
+    String jsonPayload = "{";
+    jsonPayload += "\"deviceID\": \"" + String(deviceID) + "\", ";  // deviceID should be a string in JSON
+    jsonPayload += "\"data\": [";
+    jsonPayload += "\"{\\\"temp\\\": " + String(t) + ", \\\"humidity\\\": " + String(h) + "}\"";
+    jsonPayload += "]";
+    jsonPayload += "}";
 
-  //   String jsonPayload = "{";
-  //   jsonPayload += "\"deviceID\": \"" + String(deviceID) + "\", ";  // deviceID should be a string in JSON
-  //   jsonPayload += "\"data\": [";
-  //   jsonPayload += "\"{\\\"temp\\\": " + String(temp) + ", \\\"humidity\\\": " + String(humidity) + "}\"";
-  //   jsonPayload += "]";
-  //   jsonPayload += "}";
+    int httpResponseCode = http.POST(jsonPayload);
 
-  //   int httpResponseCode = http.POST(jsonPayload);
+    if (httpResponseCode > 0) {
+      Serial.println("HTTP Response code: " + String(httpResponseCode));
+      Serial.println("Response: " + http.getString());
+    } else {
+      Serial.println("Error on sending POST: " + String(httpResponseCode));
+    }
 
-  //   if (httpResponseCode > 0) {
-  //     Serial.println("HTTP Response code: " + String(httpResponseCode));
-  //     Serial.println("Response: " + http.getString());
-  //   } else {
-  //     Serial.println("Error on sending POST: " + String(httpResponseCode));
-  //   }
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
 
-  //   http.end();
-  // } else {
-  //   Serial.println("WiFi Disconnected");
-  // }
-
-  // delay(1000*60*60);
+  delay(1000*10);
 }
